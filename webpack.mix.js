f0b731f13b50f6602743c27684aaeccf5c182018
emanuele <emattina97@gmail.com>
2021-04-18 20:25:49 +0200

    Carico bozza main

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,8 @@
 let mix = require('laravel-mix');
 
 mix.sass('src/scss/master.scss', 'dist/css')
+
+mix.options({
+    // Don't perform any css url rewriting by default
+    processCssUrls: false,
+})
